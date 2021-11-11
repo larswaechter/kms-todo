9bf69835dbf9753fd7e59dd14149a08ce4094000
julian <julian.scheffler@mni.thm.de>
2021-11-11 14:09:52 +0100

    tests

--- todo-firebase/src/app/add-todo/add-todo.component.spec.ts
+++ todo-firebase/src/app/add-todo/add-todo.component.spec.ts
@@ -37,17 +37,8 @@ describe('AddTodoComponent', () => {
     ref.componentInstance.save();
   });
 
-  /*
-  it('should not add empty todo', () => {
-    const ref = service.open(AddTodoComponent);
-
-    const cb = () => {};
-    ref.result.then(cb);
-
-    expect(cb).toHaveBeenCalledTimes(0);
-
-    ref.componentInstance.title = '';
-    ref.componentInstance.save();
+  it('should be empty', () => {
+    expect(component.title).toBe('');
   });
-  */
+
 });
