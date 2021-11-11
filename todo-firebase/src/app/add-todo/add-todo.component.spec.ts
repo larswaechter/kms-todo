333a173385799cc9edf05e60307cdc2b36751fc2
Lars Wächter <lars.waechter@mni.thm.de>
2021-11-11 13:21:54 +0100

    Add test for "add-todo" component

--- todo-firebase/src/app/add-todo/add-todo.component.spec.ts
+++ todo-firebase/src/app/add-todo/add-todo.component.spec.ts
@@ -1,16 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 import { AddTodoComponent } from './add-todo.component';
 
 describe('AddTodoComponent', () => {
   let component: AddTodoComponent;
   let fixture: ComponentFixture<AddTodoComponent>;
+  let service: NgbModal;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AddTodoComponent ]
-    })
-    .compileComponents();
+      providers: [NgbModal, NgbActiveModal],
+      declarations: [AddTodoComponent],
+    }).compileComponents();
+    service = TestBed.inject(NgbModal);
   });
 
   beforeEach(() => {
@@ -22,4 +25,29 @@ describe('AddTodoComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should add new todo', () => {
+    const ref = service.open(AddTodoComponent);
+
+    ref.result.then((res) => {
+      expect(res).toBe('Mein neues Todo');
+    });
+
+    ref.componentInstance.title = 'Mein neues Todo';
+    ref.componentInstance.save();
+  });
+
+  /*
+  it('should not add empty todo', () => {
+    const ref = service.open(AddTodoComponent);
+
+    const cb = () => {};
+    ref.result.then(cb);
+
+    expect(cb).toHaveBeenCalledTimes(0);
+
+    ref.componentInstance.title = '';
+    ref.componentInstance.save();
+  });
+  */
 });
