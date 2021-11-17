898ab223035c254c04f90066cbf08b2b4823e324
Lars Wächter <lars.waechter@mni.thm.de>
2021-11-17 17:48:02 +0100

    Split actions

--- todo-firebase/src/environments/environment.prod.ts
+++ todo-firebase/src/environments/environment.prod.ts
@@ -2,11 +2,11 @@ export const environment = {
   production: true,
   firebase: {
     apiKey: 'AIzaSyCSoBOK8XZFjMpKgWIomSdEbdrtklNqfc4',
-    authDomain: 'todo-wbs2.firebaseapp.com',
+    authDomain: 'larswaechter.github.io',
     projectId: 'todo-wbs2',
     storageBucket: 'todo-wbs2.appspot.com',
     messagingSenderId: '606503185313',
     appId: '1:606503185313:web:64ec25692e4474c0f1f9e0',
-    measurementId: 'G-V05EK5PW3R'
-  }
+    measurementId: 'G-V05EK5PW3R',
+  },
 };
